f6380eb63e6890a9f7d7a917b2e1f900506ba35f
Dhruv Sharma <dhruvsharma0503@gmail.com>
2019-10-19 23:36:30 -0700

    Added name/email info

--- lab1B/signal.c
+++ lab1B/signal.c
@@ -1,3 +1,7 @@
+// NAME: Dhruv Sharma
+// EMAIL: dhruvsharma78@ucla.edu
+// ID: 605082988
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
--- lab1B/signal.h
+++ lab1B/signal.h
@@ -1,3 +1,7 @@
+// NAME: Dhruv Sharma
+// EMAIL: dhruvsharma78@ucla.edu
+// ID: 605082988
+
 #ifndef SIGNAL_H
 #define SIGNAL_H
 
